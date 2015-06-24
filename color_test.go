8fa27913264addfb2d21e519a2f70f83fe15a6b4
francismakes <hello@francismakes.com>
2015-06-24 15:06:47 +0000

    (test) stub out units for color and group

---
+++ color_test.go
@@ -0,0 +1,11 @@
+package ase
+
+import "testing"
+
+func TestColorStruct(t *testing.T) {
+  	t.SkipNow()  
+}
+
+func TestColorRead(t *testing.T) {
+	t.SkipNow()
+}
---
+++ group_test.go
@@ -0,0 +1,11 @@
+package ase
+
+import "testing"
+
+func TestGroupStruct(t *testing.T) {
+  	t.SkipNow()  
+}
+
+func TestGroupRead(t *testing.T) {
+	t.SkipNow()
+}
