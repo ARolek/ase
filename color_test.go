de73de994eeebcc1c673178180eb7655314d9fea
francismakes <hello@francismakes.com>
2015-06-24 15:23:20 +0000

    (test) write GroupStruct units

--- color_test.go
+++ color_test.go
@@ -3,35 +3,35 @@ package ase
 import "testing"
 
 func TestColorStruct(t *testing.T) {
-    c := new(Color)
-    
+	c := new(Color)
+
 	var defaultNameLen uint16
-	var defaultName    string
-	var defaultModel   string
-	var defaultType    string
-	
+	var defaultName string
+	var defaultModel string
+	var defaultType string
+
 	if c.NameLen != defaultNameLen {
-	    t.Error("expected default Name Length of", defaultNameLen,
-	        " got ", c.NameLen)
+		t.Error("expected default Name Length of", defaultNameLen,
+			" got ", c.NameLen)
 	}
-	
+
 	if c.Name != defaultName {
-	    t.Error("expected default Name of", defaultName,
-	        " got ", c.Name)
+		t.Error("expected default Name of", defaultName,
+			" got ", c.Name)
 	}
-	
+
 	if c.Model != defaultModel {
-	    t.Error("expected default Model of", defaultModel,
-	        " got ", c.Model)
+		t.Error("expected default Model of", defaultModel,
+			" got ", c.Model)
 	}
-	
+
 	if len(c.Values) != 0 {
-	    t.Error("expected empty set of Values by default")   
+		t.Error("expected empty set of Values by default")
 	}
-	
+
 	if c.Type != defaultType {
-	    t.Error("expected default Type of", defaultType,
-	        " got ", c.Type)
+		t.Error("expected default Type of", defaultType,
+			" got ", c.Type)
 	}
 }
 
--- group_test.go
+++ group_test.go
@@ -3,7 +3,22 @@ package ase
 import "testing"
 
 func TestGroupStruct(t *testing.T) {
-  	t.SkipNow()  
+	g := new(Group)
+
+	var defaultNameLen uint16
+	var defaultName string
+
+	if g.NameLen != defaultNameLen {
+		t.Error("expected block to have default Name Len of", defaultNameLen)
+	}
+
+	if g.Name != defaultName {
+		t.Error("expected block to have default Name of", defaultName)
+	}
+
+	if len(g.Colors) != 0 {
+		t.Error("expected Colors to be initially empty")
+	}
 }
 
 func TestGroupRead(t *testing.T) {
