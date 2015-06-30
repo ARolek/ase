8b7c4de696aa4c72607a6748f83350adbdfd6ad3
francismakes <hello@francismakes.com>
2015-06-30 23:46:42 +0000

    (test) units check every color and groups present in the ase

--- ase_test.go
+++ ase_test.go
@@ -9,7 +9,6 @@ func TestDecode(t *testing.T) {
 	ase := ASE{}
 	ase.Decode(filePath, false)
 
-	// Check that each respective field is correctly populated
 	expectedSignature := "ASEF"
 	if ase.Signature != expectedSignature {
 		t.Error("expected signature of ASEF, got ", ase.Signature)
@@ -28,41 +27,111 @@ func TestDecode(t *testing.T) {
 	}
 
 	// Colors
-	expectedColor := Color{
-		NameLen: 4,
-		Name:    "RGB",
-		Model:   "RGB",
-		Values:  []float32{1, 1, 1},
-		Type:    "Normal",
+	expectedColors := []Color{
+		Color{
+			NameLen: 4,
+			Name:    "RGB",
+			Model:   "RGB",
+			Values:  []float32{1, 1, 1},
+			Type:    "Normal",
+		},
+		Color{
+			NameLen: 10,
+			Name:    "Grayscale",
+			Model:   "CMYK",
+			Values:  []float32{0, 0, 0},
+			Type:    "Spot",
+		},
+		Color{
+			NameLen: 5,
+			Name:    "cmyk",
+			Model:   "CMYK",
+			Values:  []float32{0, 1, 0},
+			Type:    "Spot",
+		},
+		Color{
+			NameLen: 4,
+			Name:    "LAB",
+			Model:   "RGB",
+			Values:  []float32{0, 0.6063648, 0.524658},
+			Type:    "Global",
+		},
+		Color{
+			NameLen: 16,
+			Name:    "PANTONE P 1-8 C",
+			Model:   "LAB",
+			Values:  []float32{0.9137255, -5, 94},
+			Type:    "Spot",
+		},
+		Color{
+			NameLen: 4,
+			Name:    "Red",
+			Model:   "RGB",
+			Values:  []float32{1, 0, 0},
+			Type:    "Global",
+		},
+		Color{
+			NameLen: 6,
+			Name:    "Green",
+			Model:   "RGB",
+			Values:  []float32{0, 1, 0},
+			Type:    "Global",
+		},
+		Color{
+			NameLen: 5,
+			Name:    "Blue",
+			Model:   "RGB",
+			Values:  []float32{0, 0, 1},
+			Type:    "Global",
+		},
 	}
 
-	firstColor := ase.Colors[0]
+	expectedNumColors := len(expectedColors)
+	actualNumColors := len(ase.Colors)
 
-	if firstColor.NameLen != expectedColor.NameLen {
-		t.Error("expected initial color of name length ", expectedColor.NameLen,
-			"got ", firstColor.NameLen)
+	if actualNumColors != expectedNumColors {
+		t.Error("expected number of colors to be", expectedNumColors,
+			"got ", actualNumColors)
 	}
 
-	if firstColor.Name != expectedColor.Name {
-		t.Error("expected initial color with name ", expectedColor.Name,
-			"got ", firstColor.Name)
-	}
+	for i, color := range ase.Colors {
+		expectedColor := expectedColors[i]
 
-	if firstColor.Model != expectedColor.Model {
-		t.Error("expected initial color of Model ", expectedColor.Model,
-			"got ", firstColor.Model)
-	}
+		if color.NameLen != expectedColor.NameLen {
+			t.Error("expected initial color of name length ", expectedColor.NameLen,
+				"got ", color.NameLen)
+		}
 
-	for i, _ := range expectedColor.Values {
-		if firstColor.Values[i] != expectedColor.Values[i] {
-			t.Error("expected color value ", expectedColor.Values[i],
-				"got ", firstColor.Values[i])
+		if color.Name != expectedColor.Name {
+			t.Error("expected initial color with name ", expectedColor.Name,
+				"got ", color.Name)
+		}
+
+		if color.Model != expectedColor.Model {
+			t.Error("expected initial color of Model ", expectedColor.Model,
+				"got ", color.Model)
+		}
+
+		for j, _ := range expectedColor.Values {
+			if color.Values[j] != expectedColor.Values[j] {
+				t.Error("expected color value ", expectedColor.Values[j],
+					"got ", color.Values[j])
+			}
+		}
+
+		if color.Type != expectedColor.Type {
+			t.Error("expected color type ", expectedColor.Type,
+				"got ", color.Type)
 		}
 	}
 
-	if firstColor.Type != expectedColor.Type {
-		t.Error("expected color type ", expectedColor.Type,
-			"got ", firstColor.Type)
+	// Groups
+	expectedGroupLen := 0
+	actualGroupLen := len(ase.Groups)
+
+	if expectedGroupLen != actualGroupLen {
+		t.Error("expected group length of ", expectedGroupLen,
+			"got ", actualGroupLen)
 	}
 
 }
